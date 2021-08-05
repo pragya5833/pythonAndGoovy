15b1341cdd6b4816760a55f36a7e0617a76c58f3
pragya bharti <pragyabharti@pragyas-MacBook-Air.local>
2021-08-05 18:20:03 +0530

    html dynamic

---
+++ dynamichtml.groovy
@@ -0,0 +1,13 @@
+String displayText = 'default'
+if(nametwo == 'my' ) {
+ displayText = 'my text'
+}
+if(nametwo == 'name' ) {
+ displayText = 'name text'
+}
+if(nametwo == 'is' ) {
+ displayText = 'is text'
+}
+
+String contents='<h2>' +displayText + '</h2>'
+return contents
